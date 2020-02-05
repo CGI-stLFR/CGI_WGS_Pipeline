a3b63b4394e3f1d36d82e812f397e9c9fb8b7c3b
Ellis-Anderson <eaanderson@ucdavis.edu>
2020-02-04 16:10:48 -0800

    added README

--- Data_and_Tools/tools/generate_summary.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-awk -F $'\t' '(NR>1){barcodecount++;fragonbarcode=fragonbarcode+$2;barcodereadcount=barcodereadcount+$3}END{ave_fragonbarcode=fragonbarcode/barcodecount;ave_barcodereadcount=barcodereadcount/barcodecount;print "barcodecount","ave_fragonbarcode","ave_barcodereadcount";print barcodecount,ave_fragonbarcode,ave_barcodereadcount}' OFS="\t" $1 > $2
--- Data_and_Tools/tools/get_readcounts.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-awk -F $'\\t' '(NR>1){fragcount++;fraglength=fraglength+$6;fragreadcount=fragreadcount+$7}END{ave_fraglen=fraglength/fragcount;ave_fragreadcount=fragreadcount/fragcount;print "fragcount","ave_fraglen","ave_fragreadcount";print fragcount,ave_fraglen,ave_fragreadcount}' OFS="\t" $1 > $2
