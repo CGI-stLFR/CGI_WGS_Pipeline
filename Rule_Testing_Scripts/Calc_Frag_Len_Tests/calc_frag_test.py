0a84e5179c685affa9c3c058bc336972a10d8571
Ellis-Anderson <eaanderson@ucdavis.edu>
2020-09-22 20:13:46 -0700

    Added comments for Rule_Testing_Scripts

--- Rule_Testing_Scripts/Calc_Frag_Len_Tests/calc_frag_test.py
+++ Rule_Testing_Scripts/Calc_Frag_Len_Tests/calc_frag_test.py
@@ -9,10 +9,14 @@ def main():
         
 
 def test_frag_len_snakefile():
+    # Make sure we're in the appropriate directory to run this script
     cwd = Path.cwd()
     print(f"Current Directory: {cwd}", file=sys.stderr)
     target_dir = Path("Rule_Testing_Scripts/Calc_Frag_Len_Tests")
     try:
+        # expectation is that someone would attempt to run
+        # this from the main directory
+        # if so attempt to move to the Calc_Frag_Len_Tests dir
         if cwd.name != "Calc_Frag_Len_Tests":
             if target_dir.exists():
                 try:
@@ -21,6 +25,7 @@ def test_frag_len_snakefile():
                     print(f"{target_dir} exists but can't be set as current working directory", file=sys.stderr)
                     sys.exit(1)
 
+            # try another route
             elif target_dir.name.exists():
                 try:
                     os.chdir(target_dir.name)
@@ -28,6 +33,7 @@ def test_frag_len_snakefile():
                     print(f"{target_dir.name} exists but can't be set as current working directory", file=sys.stderr)
                     sys.exit(1)
 
+            # give up and exit
             else:
                 print("Can't find appropriate testing directory to test split_reads.snakefile", file=sys.stderr)
                 sys.exit(1)
@@ -43,12 +49,18 @@ def test_frag_len_snakefile():
     
 
 def setup_files(sample):
+    # create links to the test files
+    # first get the path to the test bam
     bampath = "../Test_Files/hg001.sort.rmdup.bam"
+    # resolve to it's absolute path
     bamfile = Path(bampath).resolve()
+    # add .bai for the index
     bamindex = Path(bampath + ".bai").resolve()
 
+    # Create an Align/ dir and desired bam path
     Path("Align").mkdir()
     bamlinkpath = "Align/hg001.sort.rmdup.bam"
+    # link the bam and bai at the desired location using absolute paths
     bamlink = Path(bamlinkpath).resolve()
     bamindexlink = Path(bamlinkpath + ".bai").resolve()
     bamlink.symlink_to(bamfile)
@@ -56,25 +68,32 @@ def setup_files(sample):
 
 
 def remove_files():
+    # iterate through Align/ and unlink all files
     for path in Path("./").iterdir():
         if path.is_dir() and not path.name.startswith("."):
             for sub_path in path.iterdir():
                 sub_path.unlink()
             print(f"Cleaning dir {path}")
+            # remove the Align/ directory
             path.rmdir()
 
     
 def call_frag_len_snakefile():
     try:
+        # load in config file with pyyaml
         with open("../../config.yaml", 'r') as config_path:
             config = yaml.load(config_path, yaml.SafeLoader)
 
+        # pass config ID to setup files to ensure everything is named correctly
         setup_files(config['samples']['id'])
+        # try running snakemake for calc_frag_len
         try:
             subprocess.check_call(['snakemake', '-R', 'calc_frag_len'])
+        # except called process error and note failure
         except CalledProcessError:
             print("Calc_Frag_Len test failed during execution", file=sys.stderr)
             sys.exit(1)
+    # clean mock files
     finally:
         remove_files()
         
--- Rule_Testing_Scripts/Split_Read_Snakefile_Tests/split_reads_test.py
+++ Rule_Testing_Scripts/Split_Read_Snakefile_Tests/split_reads_test.py
@@ -43,17 +43,23 @@ def test_splitreads_snakefile():
     
 
 def setup_files(config):
+    # set the read directory path and resolve it to its absolute path
     reads_path = "../Test_Files/1000count"
     reads_dir = Path(reads_path).resolve()
+    # Make the samples fq directory
     Path(config['samples']['fq_path']).mkdir()
+    # create the symlink target and resolve it
     reads_link_path = Path(config['samples']['fq_path']) / config['samples']['lanes'][0]
     reads_link = reads_link_path.resolve()
+    # symlink the directory as the lane within the fq_path dir
     reads_link.symlink_to(reads_dir)
     
 
 def clean_files(config):
+    # unlink the symlinked lane
     reads_link_path = Path(config['samples']['fq_path']) / config['samples']['lanes'][0]
     reads_link_path.unlink()
+    # remove the fq_path dir
     Path(config['samples']['fq_path']).rmdir()
 
     
@@ -62,9 +68,12 @@ def call_splitreads_snakefile():
         config = yaml.load(config_path, yaml.SafeLoader)
 
     try:
+        # attempt to setup files
+        # clean if fails
         setup_files(config)
 
         try:
+            # attempt to run snakemake with rule split_reads
             subprocess.check_call(['snakemake', '-R', 'split_reads'])
         except CalledProcessError:
             print("Splitreads test failed during execution", file=sys.stderr)
@@ -72,44 +81,58 @@ def call_splitreads_snakefile():
     finally:
         clean_files(config)
         
+
+    # Test to make sure the correct output files 
+    # are generated as expected
     file_tests = {}
+    # Make sure there's a directory )
     data_path = Path("data")
     print(os.path.isdir(data_path))
+    # Note success of first test
     if os.path.isdir(data_path):
         file_tests['data'] = True
     else:
         file_tests['data'] = False
+    # check for the correctly linked starting files
     for i in ["1", "2"]:
         link = "read_" + i + ".fq.gz"
         link_path = data_path / link 
         print(os.path.islink(link_path))
+        # Note outcome of test, clean file if True
         if os.path.islink(link_path):
             file_tests['read' + i] = True
             subprocess.call(['unlink', link_path])
         else:
             file_tests['read' + i] = False
-          
+        
+        # check for the correct output files
         split_read = "split_read." + i + ".fq.gz"
         split_read_path = data_path / split_read
         print(os.path.isfile(split_read_path))
+        # note outcome of test, clean if True
         if os.path.isfile(split_read_path):
             file_tests['split_read' + i] = True
             subprocess.call(['rm', split_read_path])
         else:
             file_tests['split_read' + i] = False
             
+    # remove data/
     subprocess.call(['rm', '-r', data_path])
     
-    split_stat_path = Path("split_stat_read1.log") 
+    # check log file exists
+    split_stat_path = Path("split_stat_read1.log")
+    # Note outcome and clean
     if os.path.isfile(split_stat_path):
         file_tests['split_err'] = True
         subprocess.call(['rm',  split_stat_path])
     else:
         file_tests['split_err'] = False
+    # Print all test results
     print("Splitreads file output:", file=sys.stderr)
     for key, value in file_tests.items():
         print(f"{key}: {value}")
         
+    # Print overall success
     if not all(file_tests.values()):
         print("Splitreads file output failed", file=sys.stderr)
         sys.exit(1)
